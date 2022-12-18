e7b2c1cb6bcf8959ba2f1907d0631dffe1b6dbb1
June Balter <junedoeswork@gmail.com>
2022-12-18 17:23:48 -0600

    full title and write letter complete

--- functions.py
+++ functions.py
@@ -171,6 +171,9 @@ print(f'{sign} {parity}')
 #    parameters, making it so the job title defaults to "Engineer" if a job
 #    title is not passed in. Return the person's title and name in one string.
 
+def full_title(name, title = "Engineer"):
+    return f'{title} {name}'
+
 # 2. Write a function called write_letter that, given a recipient name & job
 #    title and a sender name, prints the following letter:
 #
@@ -180,6 +183,11 @@ print(f'{sign} {parity}')
 #    Use the function from #1 to construct the full title for the letter's
 #    greeting.
 
+def write_letter(name, title, sender):
+    
+    salutation = full_title(name, title)
+
+    print(f'Dear {salutation}, I think you are amazing! Sincerely, {sender}')
 
 ###############################################################################
 
