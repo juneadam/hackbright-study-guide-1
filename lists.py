eb95bd1b649e39d46bcfb46351d531f17979818d
June Balter <junedoeswork@gmail.com>
2022-12-18 15:10:32 -0600

    reverse

--- lists.py
+++ lists.py
@@ -217,6 +217,7 @@ def mult_numbers(numbers):
         1
     """
 
+
     return None
 
 
@@ -302,7 +303,9 @@ def reverse_list(items):
         ['apple', 'berry', 'cherry']
     """
 
-    return []
+    reversed_list = items[::-1]
+
+    return reversed_list
 
 
 def reverse_list_in_place(items):
