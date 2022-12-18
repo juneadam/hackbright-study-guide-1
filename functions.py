419111a525c2f56a28542e025b66ccb8e6e0addc
June Balter <junedoeswork@gmail.com>
2022-12-18 17:03:57 -0600

    hello, say hi, print product complete

--- functions.py
+++ functions.py
@@ -71,14 +71,20 @@ PART TWO:
 # 1. Write a function called 'hello_world' that does not take any arguments and
 #    prints "Hello World".
 
+def hello_world():
+    print("Hello World")
 
 # 2. Write a function called 'say_hi' that takes a name as a string and
 #    prints "Hi" followed by the name.
 
+def say_hi(name):
+    print(f"Hi {name}")
 
 # 3. Write a function called 'print_product' that takes two integers and
 #    multiplies them together. Print the result.
 
+def print_product(a, b):
+    print(a * b)
 
 # 4. Write a function called 'repeat_string' that takes a string and an integer
 #    and prints the string that many times
